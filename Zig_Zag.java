dadff3b717ead9203590bde6234cffa2cfdb652c
shashwatjainr <shashwat.rjain@gmail.com>
2025-08-27 23:43:56 +0530

    Add files via upload

---
+++ Zig_Zag.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Zig_Zag {
+    public static void main(String []args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }   
+    
+    public static void pzz(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println("Pre " + n);
+        pzz(n-1);
+        System.out.println("In " + n);
+        pzz(n-1);
+        System.out.println("Post " + n);
+    }
+}
