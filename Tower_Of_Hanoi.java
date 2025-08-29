5c039af3bbfa1bc80cc4b764e75475116e93090c
shashwatjainr <shashwat.rjain@gmail.com>
2025-08-29 23:57:43 +0530

    Add files via upload

---
+++ Tower_Of_Hanoi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tower_Of_Hanoi {
+    public static void main(String [] args){
+
+    }
+
+    public static void TOH(int n, int a, int b, int c){
+        
+    }
+}
