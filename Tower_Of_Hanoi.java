20a91f82a91e0fb031ee363436b9550ff9b2d129
shashwatjainr <shashwat.rjain@gmail.com>
2025-09-02 21:45:05 +0530

    Update Tower_Of_Hanoi.java

--- Tower_Of_Hanoi.java
+++ Tower_Of_Hanoi.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class Tower_Of_Hanoi {
     public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1 = scn.nextInt();
+        int t2 = scn.nextInt();
+        int t3 = scn.nextInt();
+
+        TOH(n,t1, t2, t3);
 
     }
 
-    public static void TOH(int n, int a, int b, int c){
-        
+    public static void TOH(int n, int tid1, int tid2, int tid3){
+        if(n==0){return;}
+        TOH(n-1, tid1, tid3, tid2);
+        System.out.println(n + "[" + tid1 + " -> " + tid2 + "]");
+        TOH(n-1, tid3, tid2, tid1);        
     }
 }
